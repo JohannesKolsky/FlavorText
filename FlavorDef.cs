804a830c69aa4e8d71aea79d55764cd21f1ff6d8
Nathan <Nathan@NATHANACER>
2024-06-28 15:23:59 -0700

    Add project files.

---
+++ FlavorDef.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace FlavorText
+{
+    public class FlavorDef : Def
+    {
+        public List<string> ingredients;
+    }
+}
