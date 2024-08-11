e1be8b521ac384e37936d5e9cf205a361760e67d
Nathan <ngdozet@gmail.com>
2024-08-11 00:38:54 -0700

    using RecipeDef methods

--- FlavorDef.cs
+++ FlavorDef.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using RimWorld;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using Verse;
+using HarmonyLib;
+using RimWorld;
+using System;
+using System.Linq;
+using System.Text;
+using UnityEngine;
 
 namespace FlavorText
 {
-    public class FlavorDef : Def
+
+    public class FlavorDef : RecipeDef
     {
-        public List<string> foodStuffs;
+        public int totalDepth;  // total score of how deep matching ThingCategoryDefs are in their tree, used to pick best FlavorDef
+
+/*        public List<string> flavorCategories;
+
+        private List<string> GetFlavorCategories()
+        {
+            List<string> categoriesList = [];
+            foreach (IngredientCount ing in ingredients)
+            {
+                for (int f = 0; f < ing.filter.AllowedDefCount; f++)
+                {
+                    // find the categories of the FlavorDef and make them into a new list
+                    FieldInfo categoriesField = AccessTools.Field(typeof(ThingFilter), "categories");
+                    categoriesList = (List<string>)categoriesField.GetValue(categoriesField);
+                }
+            }
+            return categoriesList;
+
+        }
+
+        public FlavorDef()
+        {
+            flavorCategories = this.GetFlavorCategories();
+        }*/
     }
 }
