d3d37135e976ae2778ab40cd3281ea7fd81a4c72
Nathan <ngdozet@gmail.com>
2024-09-08 00:20:09 -0700

    Sep 8 finished adding specificity

--- FlavorText.cs
+++ FlavorText.cs
@@ -110,10 +110,9 @@ namespace FlavorText
             return finalFlavorDef;*/
 
             FlavorDef bestFlavorDef = null;
-            if (matchingFlavorDefs.Count > 0)
+             if (matchingFlavorDefs.Count > 0)
             {
                 bestFlavorDef = ChooseBestFlavorDef(ingredientsToSearchFor, matchingFlavorDefs);
-                Log.Message("first ingredient cat is: " + bestFlavorDef.ingredients[0].filter.OverrideRootNode);
             }
             if (bestFlavorDef != null)
             {
@@ -157,11 +156,6 @@ namespace FlavorText
                 }
             }
             return matches;
-        }
-
-        public class Matches
-        {
-
         }
         
         private FlavorDef ChooseBestFlavorDef(List<ThingDef> ingredientsToSearchFor, List<FlavorDef> validFlavorDefs)  // rank valid flavor defs to choose the best
