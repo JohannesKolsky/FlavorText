d0808f145502f56693b480cd78bbb9b352271b9e
Nathan <ngdozet@gmail.com>
2024-09-07 22:37:10 -0700

    added sorting FlavorDefs by specificity

--- FlavorDef.cs
+++ FlavorDef.cs
@@ -1,41 +1,69 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using Verse;
-using HarmonyLib;
+﻿using JetBrains.Annotations;
 using RimWorld;
-using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using UnityEngine;
+using Verse;
+
+//TODO: recipe parent hierarchy
+//TODO: stuffCategoriesToAllow?
+
 
 namespace FlavorText
 {
+    /// <summary>
+    /// Effectively recipes, and is a subclass of RecipeDef. These show what combination of ingredients/ingredient categories will be given each particular name.
+    /// </summary>
+    /// 
+    public class FlavorThingFilter : ThingFilter
+    {
 
+    }
     public class FlavorDef : RecipeDef
     {
-        public int totalDepth;  // total score of how deep matching ThingCategoryDefs are in their tree, used to pick best FlavorDef
 
-/*        public List<string> flavorCategories;
-
-        private List<string> GetFlavorCategories()
-        {
-            List<string> categoriesList = [];
-            foreach (IngredientCount ing in ingredients)
-            {
-                for (int f = 0; f < ing.filter.AllowedDefCount; f++)
-                {
-                    // find the categories of the FlavorDef and make them into a new list
-                    FieldInfo categoriesField = AccessTools.Field(typeof(ThingFilter), "categories");
-                    categoriesList = (List<string>)categoriesField.GetValue(categoriesField);
-                }
-            }
-            return categoriesList;
-
-        }
-
+        public int specificity = -1;
         public FlavorDef()
         {
-            flavorCategories = this.GetFlavorCategories();
-        }*/
+            foreach (IngredientCount ingredientCount in this.ingredients)  // calculate how many ThingDefs satisfy this flavor name, the fewer the more specific and therefore better the flavor name is
+            {
+                specificity += ingredientCount.filter.AllowedDefCount;
+            }
+        }
+
+
+
+        private List<ThingCategoryDef> CategoryChain(ThingDef thingDef)  // make a list from the current ingredient to the Root of ThingCategories
+        {
+            if (thingDef.thingCategories.Count > 1) // DEBUG: if ingredient has multiple categories make an Error Log of which ones they are
+            {
+                string allCats = "";
+                for (int i = 0; i < thingDef.thingCategories.Count; i++)
+                {
+                    allCats += thingDef.thingCategories[i].defName;
+                    if (i != thingDef.thingCategories.Count) { allCats += ", "; }
+                }
+                Log.Error("ingredient belongs in multiple categories: " + allCats);
+            }
+
+            List<ThingCategoryDef> categoryChain = [];
+            ThingCategoryDef currentCat = thingDef.thingCategories.FirstOrDefault();  // (probably will need to change this allow multiple categories)
+            while (currentCat != DefDatabase<ThingCategoryDef>.GetNamed("Root"))
+            {
+                categoryChain.Add(currentCat);
+                currentCat = currentCat.parent;
+            }
+            return categoryChain;
+        }
+
+        private List<ThingCategoryDef> CategoryChain(ThingCategoryDef currentCat)  // make a list from the current ingredient to the Root of ThingCategories
+        {
+            List<ThingCategoryDef> categoryChain = [];
+            while (currentCat != DefDatabase<ThingCategoryDef>.GetNamed("Root"))
+            {
+                categoryChain.Add(currentCat);
+                currentCat = currentCat.parent;
+            }
+            return categoryChain;
+        }
     }
 }
