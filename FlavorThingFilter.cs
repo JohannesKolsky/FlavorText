239af5e64e7f4f627dc113bcd2a6dcdbaf5d067b
Nathan <ngdozet@gmail.com>
2024-09-15 16:34:54 -0700

    semi-stable, placeholders throw errors

---
+++ FlavorThingFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FlavorText
+{
+    public class FlavorThingFilter : ThingFilter
+    {
+        public List<ThingDef> thingDefs;
+        public List<string> categories;
+
+        public FlavorThingFilter() { }
+    }
+}
