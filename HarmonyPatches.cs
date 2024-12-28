19698012b196d23be46690fd7e128e83a2dbc934
Nathan <ngdozet@gmail.com>
2024-12-28 02:44:56 -0800

    v0.1 plur is now generative too

--- HarmonyPatches.cs
+++ HarmonyPatches.cs
@@ -14,27 +14,29 @@ public static class HarmonyPatches
     {
         patchType = typeof(HarmonyPatches);
         Harmony harmony = new("rimworld.hekmo.FlavorText");
-        harmony.Patch(AccessTools.Method(typeof(GenRecipe), "MakeRecipeProducts", null, null), null, new HarmonyMethod(patchType, "MakeRecipeProductsPostFix", null),null, null);
+        harmony.Patch(AccessTools.Method(typeof(GenRecipe), "MakeRecipeProducts", null, null), null, new HarmonyMethod(patchType, "MakeRecipeProductsPostFix", null), null, null);
     }
 
     // retrieve list of ingredients from meal as it's being made
     public static IEnumerable<Thing> MakeRecipeProductsPostFix(IEnumerable<Thing> __result, RecipeDef recipeDef, List<Thing> ingredients, Thing dominantIngredient)
     {
-        if (recipeDef.ProducedThingDef.IsWithinCategory(DefDatabase<ThingCategoryDef>.GetNamed("FoodMeals")))  // if the product is a meal, store its ingredients in its CompFlavor
+        Log.Message("Adding ingredients from MakeRecipeProducts to CompFlavor");
+        // add the used ingredients to EACH product made by the recipe (will usually be 1 product)
+        foreach (Thing product in __result)
         {
-            Log.Message("Adding ingredients from MakeRecipeProducts to CompFlavor");
-            foreach (Thing product in __result)
+            if (product.HasComp<CompFlavor>())
             {
+                Log.Message("Product with CompFlavor found");
+                Log.Message("Product ID is " + product.ThingID);
                 product.TryGetComp<CompFlavor>().Ingredients = ingredients;
-                Log.Message("product found");
-                Log.Message("product ID is " + product.ThingID);
                 product.TryGetComp<CompFlavor>().parent.BroadcastCompSignal("IngredientsRegistered");
                 yield return product;
             }
-        }
-        else  // if the product is not a meal, return it unchanged
-        {
-            foreach (Thing product in __result) { Log.Message("returning " + product.def.defName); yield return product; }
+            else
+            {
+                Log.Warning($"No CompFlavor found in {product.def.defName}, returning product unchanged");
+                yield return product;
+            }
         }
     }
 }
---
+++ Remove.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Text;
+
+//--TODO: recipe parent hierarchy
+//--TODO: spreadsheet descriptions are misaligned
+
+//TODO: blank ingredient option
+
+
+namespace FlavorText;
+
+public static class Remove  //TODO: add more special chars, like ø
+{
+    public static string RemoveDiacritics(string stIn)
+    {
+        string stFormD = stIn.Normalize(NormalizationForm.FormD);
+        StringBuilder sb = new StringBuilder();
+
+        for (int ich = 0; ich < stFormD.Length; ich++)
+        {
+            UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(stFormD[ich]);
+            if (uc != UnicodeCategory.NonSpacingMark)
+            {
+                sb.Append(stFormD[ich]);
+            }
+        }
+
+        return (sb.ToString().Normalize(NormalizationForm.FormC));
+    }
+}
