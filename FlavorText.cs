823f930715c4b4cfef50f2100e2bc8b3ec2682d8
Nathan <ngdozet@gmail.com>
2025-05-15 02:18:13 -0700

    v0.1.6

--- FlavorText.cs
+++ FlavorText.cs
@@ -1,4 +1,5 @@
-﻿using Verse;
+﻿using System.Linq;
+using Verse;
 
 namespace FlavorText;
 
@@ -9,6 +10,7 @@ public class FlavorText
     static FlavorText()
     {
         Log.Message($"[Flavor Text] mod is now active.");
+        Log.Warning($"{FlavorDef.ActiveFlavorDefs.Count()} active FlavorDefs for the current modlist found out of {DefDatabase<FlavorDef>.AllDefs.Count()} total FlavorDefs");
         /*DefDatabase<ThingCategoryDef>.ResolveAllReferences();  // TODO: this attempts to avoid having to ResolveReferences in CompFlavor, but rn I don't think this does anything*/
     }
 }
--- HarmonyPatches.cs
+++ HarmonyPatches.cs
@@ -1,7 +1,12 @@
+using System;
 using HarmonyLib;
 using RimWorld;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using UnityEngine;
 using Verse;
+using Verse.AI;
 
 //TODO: you want to find something for a ThingWithComps or ThingComp that runs once; maybe something graphics-related?
 //TODO: cover meals in inventories of spawned non-trader pawns (PawnInventoryGenerator)
@@ -27,6 +32,7 @@ namespace FlavorText;
 /// </summary>
 
 [StaticConstructorOnStartup]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
 public static class HarmonyPatches
 {
     static HarmonyPatches()
@@ -38,9 +44,12 @@ public static class HarmonyPatches
         harmony.Patch(AccessTools.Method(typeof(Building_NutrientPasteDispenser), "TryDispenseFood"), null, new HarmonyMethod(patchType, "TryDispenseFoodPostFix"));*/
         /*        harmony.Patch(AccessTools.Method(typeof(Pawn_CarryTracker), "TryStartCarry", [typeof(Thing)], null), null, new HarmonyMethod(patchType, "TryStartCarryPostFix", null), null, null);*/
         harmony.Patch(AccessTools.Method(typeof(GenRecipe), "MakeRecipeProducts"), null, new HarmonyMethod(patchType, "MakeRecipeProductsPostFix"));
+
+        /*harmony.Patch(AccessTools.Method(typeof(Toils_Ingest), "TakeMealFromDispenser"), null, new HarmonyMethod(patchType, "TakeMealFromDispenserPostFix"));
+        harmony.Patch(AccessTools.Method(typeof(JobDriver_Ingest), "GetReport"), null, new HarmonyMethod(patchType, "GetReportPostFix"));*/
     }
 
-    // ReSharper disable once InconsistentNaming
+    // dirty ingredient cache when a new ingredient is added, forcing a recheck once TryGetFlavorText is next called
     // ReSharper disable once UnusedMember.Global
     public static void RegisterIngredientPostFix(ref CompIngredients __instance)
     {
@@ -50,35 +59,11 @@ public static class HarmonyPatches
         if (compFlavor != null) compFlavor.TriedFlavorText = false;
     }
 
-    // possibly unnecessary, and also conflicts with CommonSense random ingredients which for nutrient paste are briefly added before being replaced by the actual ingredients
-    // after making a Thing, try and get flavor text if it should have flavor text
-    // things like VCE canned meat aren't included, because they do not track which meat is in them once put into a meal (e.g. canned human meat in a meal isn't abhorrent)
-    // ReSharper disable once UnusedMember.Global
-    // ReSharper disable once InconsistentNaming
-    /*public static void MakeThingPostFix(ref Thing __result)
-    {
-        if (!__result.HasComp<CompFlavor>()) return;
-        Log.Warning("MakeThing");
-        CompFlavor compFlavor = __result.TryGetComp<CompFlavor>();
-        compFlavor?.TryGetFlavorText();
-    }*/
-
-
-    // when dispensing nutrient paste, try and get flavor text
-    // ReSharper disable once UnusedMember.Global
-    // ReSharper disable once InconsistentNaming
-    /*public static void TryDispenseFoodPostFix(ref Thing __result)
-    {
-        if (!__result.HasComp<CompFlavor>()) return;
-        Log.Warning("TryDispenseFood");
-        CompFlavor compFlavor = __result.TryGetComp<CompFlavor>();
-        compFlavor?.TryGetFlavorText();
-    }*/
 
     // after making a product with CompIngredients, add information about how it was cooked
     // ReSharper disable once UnusedMember.Global
     // ReSharper disable once InconsistentNaming
-    public static IEnumerable<Thing> MakeRecipeProductsPostFix(IEnumerable<Thing> __result, IBillGiver billGiver, Pawn worker)
+    public static IEnumerable<Thing> MakeRecipeProductsPostFix(IEnumerable<Thing> __result, IBillGiver billGiver, Pawn worker, List<Thing> ingredients)
     {
         foreach (Thing product in __result)
         {
@@ -90,6 +75,11 @@ public static class HarmonyPatches
                 {
                     compFlavor.CookingStation = ((Thing)billGiver).def;
                     compFlavor.HourOfDay = GenLocalDate.HourOfDay(billGiver.Map);
+                    // average percentage of hit points of each ingredient group (ignoring quantity in group)
+                    compFlavor.IngredientsHitPointPercentage = ingredients
+                        .FindAll(i => i?.def != null && ThingCategoryDefUtility.FlavorRoot.ContainedInThisOrDescendant(i.def))
+                        .Where(i => i.def.useHitPoints)
+                        .Sum(j => (float)j.HitPoints / j.MaxHitPoints) / ingredients.Count;
                     if (ModsConfig.BiotechActive && worker.genes.HasActiveGene(DefDatabase<GeneDef>.GetNamed("Furskin")))
                     {
                         Rand.PushState(product.thingIDNumber);
@@ -104,5 +94,89 @@ public static class HarmonyPatches
             yield return product;
         }
     }
+
+    // possibly unnecessary, and also conflicts with CommonSense random ingredients which for nutrient paste are briefly added before being replaced by the actual ingredients
+    // after making a Thing, try and get flavor text if it should have flavor text
+    // things like VCE canned meat aren't included, because they do not track which meat is in them once put into a meal (e.g. canned human meat in a meal isn't abhorrent)
+    /*public static void MakeThingPostFix(ref Thing __result)
+    {
+        if (!__result.HasComp<CompFlavor>()) return;
+        Log.Warning("MakeThing");
+        CompFlavor compFlavor = __result.TryGetComp<CompFlavor>();
+        compFlavor?.TryGetFlavorText();
+    }*/
+
+
+    // when dispensing nutrient paste, try and get flavor text
+    /*public static void TryDispenseFoodPostFix(ref Thing __result)
+    {
+        if (!__result.HasComp<CompFlavor>()) return;
+        Log.Warning("TryDispenseFood");
+        CompFlavor compFlavor = __result.TryGetComp<CompFlavor>();
+        compFlavor?.TryGetFlavorText();
+    }*/
+
+
+    /*// edits job override string after getting nutrient paste meal from dispenser
+    public static void TakeMealFromDispenserPostFix(ref Toil __result)
+    {
+        /*Toil toil = __result;
+        Job curJob = toil.actor.jobs.curJob;
+        Log.Warning($"curJob is {curJob.def.defName}");
+        Log.Warning($"curToil is {toil.debugName}");
+        toil.AddFinishAction(delegate
+        {
+            Thing thing = toil.actor.carryTracker.CarriedThing;
+            if (thing?.def.ingestible != null)
+            {
+                if (!thing.def.ingestible.ingestReportStringEat.NullOrEmpty() && (thing.def.ingestible.ingestReportString.NullOrEmpty() || (int)toil.actor.RaceProps.intelligence < 1))
+                {
+                    curJob.reportStringOverride = thing.def.ingestible.ingestReportStringEat.Formatted(curJob.targetA.Thing.LabelShort, curJob.targetA.Thing);
+                }
+                if (!thing.def.ingestible.ingestReportString.NullOrEmpty())
+                {
+                    curJob.reportStringOverride = thing.def.ingestible.ingestReportString.Formatted(curJob.targetA.Thing.LabelShort, curJob.targetA.Thing);
+                }
+                Log.Warning($"TakeMealFromDispenser, report string override was {curJob.reportStringOverride}");
+            }
+        });
+        __result = toil;#1#
+        Log.Warning($"added finish action");
+    }
+
+    // changes job display string after getting nutrient paste meal from dispenser
+    public static void GetReportPostFix(ref string __result, ref JobDriver_Ingest __instance)
+    {
+        Log.Warning($"GetReport, report string override was {__instance.job.reportStringOverride}");
+        /*if (!__instance.job.reportStringOverride.NullOrEmpty())
+        {
+            return __instance.job.reportStringOverride;
+        }#1#
+        Thing thing = __instance.job.targetA.Thing;
+        if (thing?.def.ingestible != null)
+        {
+            Log.Warning($"ingestible");
+            if (!thing.def.ingestible.ingestReportStringEat.NullOrEmpty() && (thing.def.ingestible.ingestReportString.NullOrEmpty() || (int)__instance.pawn.RaceProps.intelligence < 1))
+            {
+                Log.Warning($"unintelligent");
+                __result = thing.def.ingestible.ingestReportStringEat.Formatted(__instance.job.targetA.Thing.LabelShort, __instance.job.targetA.Thing);
+            }
+            if (!thing.def.ingestible.ingestReportString.NullOrEmpty())
+            {
+                Log.Warning($"ingestible nonEmpty");
+                __result = thing.def.ingestible.ingestReportString.Formatted(__instance.job.targetA.Thing.LabelShort, __instance.job.targetA.Thing);
+            }
+        }
+
+        try
+        {
+            __result = __instance.GetReport();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex.Message);
+            __result =
+        }
+    }*/
 
 }
